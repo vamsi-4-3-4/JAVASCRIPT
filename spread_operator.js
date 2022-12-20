dc7aa27cd2625afb0a69d8afe3271d1bd12c96fa
vamsi-4-3-4 <94716145+vamsi-4-3-4@users.noreply.github.com>
2022-12-20 16:18:28 +0530

    Create spread_operator.js

---
+++ spread_operator.js
@@ -0,0 +1,10 @@
+var myArray=[1,2,3,4,5,6,7,8];
+var max=Math.max(myArray);
+var min=Math.min(myArray);
+console.log(max)
+//EVALUATING EXPRESSIONS WITH THE ES6 SPREAD OPERATOR
+var maxe=Math.max(...myArray);
+var minne=Math.min(...myArray);
+
+console.log(maxe)
+console.log(minne);
